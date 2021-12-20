787c36e682813833a8236e992be191446dd24605
Chris <christutor089@gmail.com>
2021-12-20 01:51:51 -0800

    Initialized app and began tutorial

---
+++ react-redux-typescript/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is my React - Redux - TypeScript practice
+    </div>
+  );
+}
+
+export default App;
