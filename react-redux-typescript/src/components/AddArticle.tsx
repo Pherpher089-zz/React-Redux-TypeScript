6eb6b21bbd2d8188900faea213cd8a799fba2912
Chris <christutor089@gmail.com>
2021-12-20 02:59:28 -0800

    Everything is set up accept for the app

---
+++ react-redux-typescript/src/components/AddArticle.tsx
@@ -0,0 +1,30 @@
+import React, { useState, FC, FormEvent } from 'react';
+
+type Props = {
+    saveArticle: (article: IArticle | any) => void;
+}
+
+const AddArticle: FC<Props> = ({ saveArticle }) => {
+    const [article, setArticle] = useState<IArticle | {}>();
+    const handleArticleData = (e: FormEvent<HTMLInputElement>) => {
+        setArticle({
+            ...article,
+            [e.currentTarget.id]: e.currentTarget.value,
+        })
+    }
+
+    const addNewArticle = (e: React.FormEvent) => {
+        e.preventDefault();
+        saveArticle(article);
+    }
+
+    return (
+        <form onSubmit={addNewArticle} className="Add-Article">
+            <input type="text" id="title" placeholder='Title' onChange={handleArticleData} />
+            <input type="text" id='body' placeholder='Description' onChange={handleArticleData} />
+            <button disabled={article === undefined ? true : false}>Add Article</button>
+        </form>
+    )
+}
+
+export default AddArticle
